8fe3037bbba4e41329c240ffdfe1ed272184d148
Jonas_Jones <me@jonasjones.dev>
2024-12-23 04:58:37 +0100

    added rick roll tracker

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		await recordRequest(request, env);
+		return Response.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", 302);
 	},
 };
+
+async function recordRequest(request, env) {
+
+	const url = new URL(request.url);
+
+	const analyticsData = {
+	  timestamp: Date.now(),
+	  domain: url.hostname,
+	  method: request.method,
+	  path: url.pathname,
+	  country: request.cf.country,
+	}
+	console.log('Recording request:', analyticsData);
+	const ANALYTICS_URL = 'https://analytics.jonasjones.dev/requests/record';
+
+	const response = await fetch(ANALYTICS_URL, {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+		'Authorization': env.VITE_ANALYTICS_API_KEY,
+	  },
+	  body: JSON.stringify(analyticsData)
+	});
+
+	if (response.ok) {
+	  console.log('Request recorded successfully');
+	} else {
+	  console.error('Failed to record request:', response.status, await response.text());
+	}
+  }
